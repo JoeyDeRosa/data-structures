177277ee8168f9e2e659b7019a3e939041aef9b4
pasaunders <kaorti@gmail.com>
2016-12-14 13:01:17 -0800

    Push and append methods pass tests.

---
+++ src/double_linked_list.py
@@ -0,0 +1,103 @@
+"""Creats a double linked list."""
+
+
+class DoubleLinkedList(object):
+    """A singly-linked list."""
+
+    def __init__(self, data=None):
+        """Create an instance of type LinkedList. Allow data to be passed in."""
+        self.head = None
+        self.tail = None
+        if data is not None:
+            try:
+                for item in data:
+                    if item is data[0]:
+                        self.head = Node(item, next=None, prev=None)
+                        self.tail = self.head
+                    else:
+                        node = Node(item, self.head, prev=None)
+                        self.head.prev = node
+                        self.head = node
+            except TypeError:
+                node = Node(data, next=None, prev=None)
+                self.head = node
+                self.tail = self.head
+
+
+    def push(self, val):
+        """Push a val to the end of the list."""
+        node = Node(val, self.head, None)
+        if self.head is not None:
+            self.head.prev = node
+        self.head = node
+
+
+    def append(self, val):
+        """Append a val to the  start of the list."""
+        node = Node(val, None, self.tail)
+        if self.tail is not None:
+            self.tail.next = node
+        self.tail = node
+
+
+    def pop(self):
+        val = self.head.data
+        if self.head.next == None:
+            self.tail = None
+        self.head = self.head.next
+        return val
+
+    def size(self):
+        a = self.head
+        count = 0
+        if self.head:
+            count = 1
+        while self.tail is not a:
+            count +=1
+            a = a.next
+        return count
+
+
+    def search(self, val):
+        node = self.head
+        while True:
+            if node.data == val:
+                return node
+            elif node == self.tail:
+                return None
+            node = node.next
+
+
+    def remove(self, node):
+        if node is self.head:
+            self.head = node.next
+        else:
+            a = self.head
+            while a.next is not node:
+                a = a.next
+            if node is self.tail:
+                self.tail = a
+                a.next = None
+            else:
+                a.next = node.next
+
+
+    def display(self):
+        if self.head is None:
+            return '()'
+        elif self.head is self.tail:
+            return '(' + str(self.head.data) + ')'
+        dis = '(' + str(self.head.data)
+        a = self.head
+        while a.next is not self.tail:
+            dis += ', ' + str(a.next.data)
+            a = a.next
+
+        return dis + ', ' + str(self.tail.data) + ')'
+
+
+class Node(object):
+    def __init__(self, data, next, prev):
+        self.data = data
+        self.next = next
+        self.prev = prev
---
+++ src/test_double_linked_list.py
@@ -0,0 +1,101 @@
+"""Test file for double linked list."""
+
+
+import pytest
+
+
+@pytest.fixture
+def dll():
+    """Fixture for testing iterables."""
+    from double_linked_list import DoubleLinkedList
+    return DoubleLinkedList([1, 2, 3])
+
+
+@pytest.fixture
+def empty():
+    """Fixture for testing empty instantiations."""
+    from double_linked_list import DoubleLinkedList
+    return DoubleLinkedList()
+
+
+def test_constructor(empty):
+    """Test that DoubleLinkedList constructor creates a double linked list."""
+    from double_linked_list import DoubleLinkedList
+    assert type(empty) == DoubleLinkedList
+
+
+def test_with_iterable1(dll):
+    """Test that double linked list has the correct head."""
+    assert dll.head.data is 3
+
+
+def test_prev(dll):
+    """Test that prev point to the node upwards on the chain."""
+    assert dll.head.next.prev is dll.head
+
+
+def test_tail(dll):
+    """Test that the tail has the proper value."""
+    assert dll.tail.data is 1
+
+
+def test_push_empty(empty):
+    """Test that we push to an empty list properly."""
+    empty.push(3)
+    assert empty.head.data is 3
+
+
+def test_push_empty_next(empty):
+    """Test that pushing into an empty dll has the right next value."""
+    empty.push(3)
+    assert empty.head.next is None
+
+
+def test_push_empty_prev(empty):
+    """Test that pushing into an empty dll has the right prev value."""
+    empty.push(3)
+    assert empty.head.prev is None
+
+
+def test_push_full(dll):
+    """Test that pushing into a list with values works."""
+    dll.push(6)
+    assert dll.head.data is 6
+
+
+def test_push_full_next(dll):
+    """Test that pushing into a list with values gives the proper next value."""
+    dll.push(6)
+    assert dll.head.next.data is 3
+
+
+def test_append(empty):
+    """Test that appending to an empty list stores the right value."""
+    empty.append(6)
+    assert empty.tail.data is 6
+
+
+def test_append_value(dll):
+    """Test that appending to a list with values stores the right value."""
+    dll.append(6)
+    assert dll.tail.data is 6
+
+
+
+def test_append_next(dll):
+    """Test that appending to a list with values has the right next value."""
+    dll.append(6)
+    assert dll.tail.next is None
+
+
+
+def test_append_prev(dll):
+    """Test that appending to a list with values has the right prev value."""
+    dll.append(6)
+    assert dll.tail.prev.data is 1
+
+
+def test_append_tail_reassign(dll):
+    """Test that the old tail's next value is reassigned."""
+    dll.append(6)
+    assert dll.tail.prev.next is dll.tail
