b5a303c9e8332322fb146386d18a9bd343bdea0a
regenalgrant <regenal@mac.com>
2016-12-19 16:17:16 -0800

    add test deque

--- src/test_deque.py
+++ src/test_deque.py
@@ -43,3 +43,40 @@ def test_pop_empty(dq):
     """Test that pop on an empty list throws an error."""
     with pytest.raises(AttributeError):
         dq.pop()
+
+
+def test_pop_left(dq):
+    """Test pop left returns the value head."""
+    dq.appendleft(4)
+    assert dq.popleft() is 4
+
+
+def test_pop_left_tail(dq):
+    """Test left pop remove tail when only a value."""
+    dq.append(4)
+    assert dq.popleft() is 4
+
+
+def test_pop_left_empty(dq):
+    """Test pop left on an empty list."""
+    with pytest.raises(AttributeError):
+        dq.popleft()
+
+
+def test_peek(dq):
+    """Test next value that would be pop without popping itself."""
+    dq.appendleft(5)
+    assert dq.dll.head.data is 5
+
+
+def test_peekleft(dq):
+    """Test for value popleft without popping."""
+    dq.appendleft(4)
+    assert dq.peekleft() is 4
+
+
+def test_size(dq):
+    """Test size returns length of the dq."""
+    dq.appendleft(4)
+    dq.append(4)
+    assert dq.size() is 2
