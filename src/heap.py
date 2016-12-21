c76980ddd150a9645c722b934687fbd0687899f0
Joey DeRosa <joeyderosa11.jd@gmail.com>
2016-12-20 19:04:02 -0800

    order list works

--- src/heap.py
+++ src/heap.py
@@ -3,16 +3,19 @@
 
 class Heap(object):
     """A heap."""
-    def __init__(self, nums=None):
+
+    def __init__(self, val=None):
         """Create the heap."""
         self.heap = []
-        if nums is None:
-            return nums
+        if val is None:
+            return val
         try:
-            for i in nums:
-                self.push(i)
-        except AttributeError:
-            print('Requires iterable.')
+            for i in val:
+                self.heap.append(i)
+            self.heap = self._organize_list(self.heap)
+        except TypeError:
+            print('Requires iterable!')
+        print(self.heap)
 
     def push(self, val):
         """Add a node containing val to the heap."""
@@ -29,31 +32,26 @@ class Heap(object):
         """Order a list in min heap format."""
         for i in range(len(lst)):
             try:
-                if lst[i].val > lst[i * 2 + 1].val:
+                if lst[i] > lst[i * 2 + 1]:
                     lst[i], lst[i * 2 + 1] = lst[i * 2 + 1], lst[i]
-                elif lst[i].val > lst[i * 2 + 2].val:
+                if lst[i] > lst[i * 2 + 2]:
                     lst[i], lst[i * 2 + 2] = lst[i * 2 + 2], lst[i]
             except IndexError:
                 continue
-        for i in range(len(lst)):
-            if lst[len(lst) - i].val < lst[(len(lst) - i) / 2].val:
-                lst[len(lst) - i], lst[(len(lst) - i) / 2 - 1] = lst[(len(lst) - i) / 2 - 1], lst[len(lst) - i]
-        return self._renode(lst)
-
-    def _renode(self, lst):
-        """Reassign the left and right values for nodes."""
+        print(self.heap)
         for i in range(len(lst)):
             try:
-                lst[i].left, lst[i].right = lst[i * 2], lst[i * 2 + 1]
+                print('index ', len(lst) - i)
+                if (len(lst) - i) % 2 is 0:
+                    print('if ', lst[(len(lst) - i)], ' compared to ', lst[((len(lst) - i) // 2) - 1])
+                    if lst[len(lst) - i] < lst[((len(lst) - i) // 2) - 1]:
+                        print(lst[(len(lst) - i)], ' switches with ', lst[((len(lst) - i) // 2) - 1])
+                        lst[len(lst) - i], lst[(len(lst) - i) // 2 - 1] = lst[(len(lst) - i) // 2 - 1], lst[len(lst) - i]
+                else:
+                    print('else ', lst[(len(lst) - i)], ' compared to ', lst[(len(lst) - i) // 2])
+                    if lst[len(lst) - i] < lst[(len(lst) - i) // 2]:
+                        print(lst[(len(lst) - i)], ' switches with ', lst[(len(lst) - i) // 2])
+                        lst[len(lst) - i], lst[(len(lst) - i) // 2] = lst[(len(lst) - i) // 2], lst[len(lst) - i]
             except IndexError:
                 continue
-
-
-class Node(object):
-    """Node containing data."""
-
-    def __init__(self, val):
-        """Create new node."""
-        self.val = val
-        self.left = None
-        self.right = None
+        return lst
