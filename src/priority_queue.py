aa82c9cc255ceaf9e280cc8f70e5e4d22717d3cd
Joey DeRosa <joeyderosa11.jd@gmail.com>
2016-12-21 16:29:16 -0800

    init and insert tested and created

--- src/priority_queue.py
+++ src/priority_queue.py
@@ -4,17 +4,27 @@
 class PriorityQueue(object):
     """A priority queue."""
 
-    def __init__(self, val, pri):
+    def __init__(self, val=None, pri=0):
         """Create an instance of a Priority Queue."""
-        pass
+        self._q = []
+        if val is None:
+            return None
+        self.insert(val, pri)
 
-    def insert(self, item):
+    def insert(self, val, pri=0):
         """Insert an item into a queue."""
-        pass
+        if type(pri) is int:
+            self._q += [(val, pri)]
+        else:
+            assert TypeError('Priority must be int.')
 
     def pop(self):
         """Remove the highest prioritized item."""
-        pass
+        pri = 0
+        for i in range(len(self._q)):
+            if self._q[pri][1] < self._q[i][1]:
+                pri = i
+        self._q.pop(i)
 
     def peek(self):
         """View the value of the highest prioritized item without removing it."""
